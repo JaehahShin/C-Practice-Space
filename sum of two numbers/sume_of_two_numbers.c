5fc2fc36cb5f7dbf73ed642849f2c49e8c2714d2
JaehahShin <114309103+JaehahShin@users.noreply.github.com>
2023-01-14 15:31:27 -0500

    Add files via upload

---
+++ sum of two numbers/sume_of_two_numbers.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+int number = 0, number1 = 0;
+printf("enter a number:");
+scanf("%d", &number);
+printf("enter a number1:");
+scanf("%d", &number1);
+printf("sum of two numbers: %d", number + number1);
+return 0;
+}
